08bed8d736b3b8f5038fc7fc2c9c0b07576abd41
moti gabay <motigabay18@gmail.com>
2025-08-17 15:30:21 +0300

    add host="0.0.0.0", port=5000, debug=True

--- app.py
+++ app.py
@@ -101,5 +101,5 @@ if __name__ == '__main__':
     app = create_app()
     with app.app_context(): # <--- זה עדיין חשוב אם אתה מריץ כך
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
